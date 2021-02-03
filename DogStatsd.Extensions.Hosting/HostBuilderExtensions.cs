4890073bf6e77e6148b14b87b4c1cb98a06ba0f7
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-02-03 13:18:58 +0100

    Updated so to add abiblity to use a contect function

--- DogStatsd.Extensions.Hosting/HostBuilderExtensions.cs
+++ DogStatsd.Extensions.Hosting/HostBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System;
+using Microsoft.Extensions.Hosting;
 
 namespace StatsdClient.Extensions.Hosting
 {
@@ -6,9 +7,31 @@ namespace StatsdClient.Extensions.Hosting
     {
         public static IHostBuilder UseDogStatsd(this IHostBuilder builder, StatsdConfig config)
         {
-            DogStatsd.Configure(config);
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            ConfigureDogStatsd(config);
 
             return builder;
+        }
+
+        public static IHostBuilder UseDogStatsd(this IHostBuilder builder, Func<HostBuilderContext, StatsdConfig> options)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.ConfigureServices((context, services) =>
+            {
+                var config = options(context);
+                ConfigureDogStatsd(config);
+            });
+
+            return builder;
+        }
+
+        private static void ConfigureDogStatsd(StatsdConfig config)
+        {
+            if (config == null) throw new ArgumentNullException(nameof(config));
+
+            DogStatsd.Configure(config);
         }
     }
 }
