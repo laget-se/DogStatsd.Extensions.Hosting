c3ac6071e812e713fc41fbc7b50b63bc43ccc929
Erik Hennerfors <erik.hennerfors@laget.se>
2022-11-09 11:09:58 +0100

    Revert "First commit"
    This reverts commit f522059f3a06d6e7cc7921b77008f1fa2194b766.

---
+++ DogStatsd.Extensions.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace StatsdClient.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseDogStatsd(this IHostBuilder builder, StatsdConfig config)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            ConfigureDogStatsd(config);
+
+            return builder;
+        }
+
+        public static IHostBuilder UseDogStatsd(this IHostBuilder builder, Func<HostBuilderContext, StatsdConfig> options)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.ConfigureServices((context, services) =>
+            {
+                var config = options(context);
+                ConfigureDogStatsd(config);
+            });
+
+            return builder;
+        }
+
+        private static void ConfigureDogStatsd(StatsdConfig config)
+        {
+            if (config == null) throw new ArgumentNullException(nameof(config));
+
+            DogStatsd.Configure(config);
+        }
+    }
+}
